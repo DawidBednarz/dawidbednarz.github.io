f588907b49c41458139f476bbc1f87fa3bbcc720
Dawid <123315624+DawidBednarz@users.noreply.github.com>
2025-12-02 00:35:39 +0100

    new web ver

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('show'); // Przełącza klasę 'show'
+}
+
+// Resetowanie widoczności menu przy powiększaniu okna
+window.addEventListener('resize', () => {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const sidenav = document.querySelector('.sidenav');
+
+    if (window.innerWidth > 768) {
+        mobileMenu.classList.remove('show'); // Usuń klasę 'show' dla mobilnego menu
+        sidenav.style.display = "block"; // Pokaż sidebar
+    } else {
+        sidenav.style.display = "none"; // Ukryj sidebar na małych ekranach
+    }
+});
