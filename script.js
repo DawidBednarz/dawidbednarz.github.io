c98f03e7ef3e789acc4c2d1e4b4cfe829c7ace57
Dawid <123315624+DawidBednarz@users.noreply.github.com>
2025-12-03 00:45:29 +0100

    update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.addEventListener('resize', () => {
     const mobileMenu = document.querySelector('.mobile-menu');
     const sidenav = document.querySelector('.sidenav');
 
-    if (window.innerWidth > 768) {
+    if (window.innerWidth > 1028) {
         mobileMenu.classList.remove('show'); // Usuń klasę 'show' dla mobilnego menu
         sidenav.style.display = "block"; // Pokaż sidebar
     } else {
