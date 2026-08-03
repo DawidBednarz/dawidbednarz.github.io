5f85603937beda6aba357b0929091aabc4bf99b1
Dawid <123315624+DawidBednarz@users.noreply.github.com>
2026-08-03 20:26:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 function toggleMenu() {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.classList.toggle('show'); // Przełącza klasę 'show'
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenu.classList.toggle('show');
 }
 
-// Resetowanie widoczności menu przy powiększaniu okna
 window.addEventListener('resize', () => {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    const sidenav = document.querySelector('.sidenav');
-
-    if (window.innerWidth > 1028) {
-        mobileMenu.classList.remove('show'); // Usuń klasę 'show' dla mobilnego menu
-        sidenav.style.display = "block"; // Pokaż sidebar
-    } else {
-        sidenav.style.display = "none"; // Ukryj sidebar na małych ekranach
-    }
+  const mobileMenu = document.querySelector('.mobile-menu');
+  if (window.innerWidth > 1028) {
+    mobileMenu.classList.remove('show');
+  }
 });
